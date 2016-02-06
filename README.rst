d65bd416da4893203b5735afdd61b0b60ff6bdbc
Martin Mahner <martin@mahner.org>
2016-02-06 17:48:39 +0100

    Initial version

---
+++ README.rst
@@ -0,0 +1,25 @@
+=========
+pip-check
+=========
+
+Gives you a quick overview of all installes packages and their update status.
+Very much like `pip list -o` but with colors. Inspired by `npm-check`_ though
+lacks most of its features (yet).
+
+.. _npm-check: https://www.npmjs.com/package/npm-check
+
+.. image:: http://d.pr/i/1e7wJ.png
+
+Installation and Usage
+----------------------
+
+::
+
+    $ pip install pip-check
+    $ pip-check
+
+Changelog
+---------
+
+v0.1 (2016-02-06):
+    Very first version, and yet with very limited features.
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from sys import exit
+
+from setuptools import find_packages, setup
+
+long_description = u'\n\n'.join((
+    open('README.rst').read(),
+))
+
+setup(
+    name='pip-check',
+    version='0.1',
+    description='Display installed pip packages and their update status..',
+    long_description=long_description,
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='https://github.com/bartTC/pip-check/',
+    classifiers=[
+    ],
+    packages=find_packages(),
+    package_data={},
+    include_package_data=True,
+    scripts = ['pip-check'],
+    install_requires=[
+        'terminaltables',
+        'colorclass',
+    ],
+    tests_require=[
+    ],
+    cmdclass={
+    },
+)
