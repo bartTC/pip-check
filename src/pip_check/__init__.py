5b43002eb81c25d5aa79ad5456de1a985db70285
Martin Mahner <martin@mahner.org>
2021-12-25 14:50:18 +0100

    Dont raise error on ctrl c

--- src/pip_check/__init__.py
+++ src/pip_check/__init__.py
@@ -390,4 +390,7 @@ def main():
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
