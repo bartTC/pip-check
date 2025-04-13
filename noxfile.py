b23840a4841d7498d952328fc356fe4562c85156
Martin Mahner <martin@mahner.org>
2025-04-13 14:22:55 +0200

    Improve testing

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import nox
+
 
 nox.options.default_venv_backend = "uv"
 
@@ -6,12 +9,18 @@ python_versions = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
 
 
 @nox.session(python=python_versions)
-def tests(session):
+def tests(session: nox.Session):
     session.install("--upgrade", "pip", "uv")
     session.install("html5lib==0.999999999", "django==1.10", "pyglet==2.0.dev23")
     session.install(".")  # Assuming pip-check is in the current project
 
-    session.run("pip-check")
+    response = session.run("pip-check", silent=True)
+
+    # Make sure, packages are actually listed
+    assert "1.10" in response
+    assert "0.999999999" in response
+    assert "2.0.dev23" in response
+
     session.run("pip-check", "--help")
     session.run(
         "pip-check",
